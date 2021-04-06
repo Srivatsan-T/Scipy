dfafd99ef457e7074c0f48b7a78fbd15066862b5
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-04-06 17:16:50 +0530

    added

--- scipy_codes.py
+++ scipy_codes.py
@@ -5,5 +5,17 @@ print(constants.kilo)
 print(constants.lb)
 print(constants.kilo)
 """
-from scipy import constants
 
+"""
+from scipy import constants
+print(constants.arcmin)
+print(constants.arcsec)
+print(constants.minute)
+print(constants.hour)
+print(constants.hectare)
+print(constants.gallon_US)
+"""
+
+from scipy import constants
+print(constants.speed_of_light)
+print(constants.speed_of_sound)
