eba349d970f8edf5af133457ee806049b5cfa576
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-04-06 17:56:08 +0530

    changes

--- scipy_codes.py
+++ scipy_codes.py
@@ -16,6 +16,30 @@ print(constants.hectare)
 print(constants.gallon_US)
 """
 
+"""
 from scipy import constants
 print(constants.speed_of_light)
 print(constants.speed_of_sound)
+print(constants.degree_Fahrenheit)
+print(constants.zero_Celsius)
+print(constants.eV)
+print(constants.light_year)
+print(constants.horsepower)
+"""
+
+"""
+from scipy.optimize import minimize
+
+def eq(x):
+    return x**2+x+2
+
+print(minimize(eq,0,method = 'BFGS').x)
+"""
+
+from scipy.sparse import csr_matrix
+import numpy as np
+a = np.array([0,1,2,0,0,0,1])
+print(csr_matrix(a))
+print(csr_matrix(a).data)
+print(csr_matrix(a).count_nonzero())
+
